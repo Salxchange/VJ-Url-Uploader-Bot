4c9800ce811b4a7495fecf1072a609612634d05c
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-02-17 22:54:37 +0100

    Update config.py

--- config.py
+++ config.py
@@ -62,10 +62,10 @@ class Config(object):
     TECH_VJ_UPDATES_CHANNEL = int(tech_vj_update_channel) if tech_vj_update_channel and id_pattern.search(tech_vj_update_channel) else None  
     
     # Url Shortner Information 
-    TECH_VJ = bool(environ.get('TECH_VJ', False)) # Set False If you want shortlink off else True
+    TECH_VJ = bool(environ.get('TECH_VJ', 'disable')) # Set False If you want shortlink off else True
     TECH_VJ_URL = environ.get('TECH_VJ_URL') # your shortlink url domain or url without https://
     TECH_VJ_API = environ.get('TECH_VJ_API') # your url shortner api
-    TECH_VJ_TUTORIAL = os.environ.get("TECH_VJ_TUTORIAL", "https://t.me/How_To_Open_Linkl")
+    TECH_VJ_TUTORIAL = os.environ.get("TECH_VJ_TUTORIAL")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
